9f2379874e62db12edc928c1fefe48e142282119
Phoenix-the-Imperial <www.tennysonbenjamin@gmail.com>
2024-07-26 02:47:02 +0530

    Update Version SPMA-0.0.0

---
+++ README.md
@@ -0,0 +1,9 @@
+# String Pattern Matching using Automaton
+
+## Author
+
+ Rishav Chakraborty
+
+## Description
+
+ Pattern matching in strings using a simulated automaton. Current Version SPMA-0.0.0.
---
+++ main.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <stdint.h>
+#include <string>
+
+class Automaton
+{
+    public:
+    char loop_char;
+    int loop_state;
+    int state;
+    int start_index;
+    std::string pattern;
+
+    public:
+    Automaton(const std::string pattern);
+    ~Automaton(void) {};
+
+    bool make_transition(const char& input_char, const size_t& current_index);
+    size_t find_first_match(const std::string& str);
+};
+
+Automaton::Automaton(const std::string pattern)
+{
+    this->state = -1;
+    this->start_index = 0;
+    this->pattern = pattern;
+    this->loop_char = pattern[0];
+    size_t i = 0;
+    while (++i < pattern.length() && pattern[i] == this->loop_char);
+    this->loop_state = i - 1;
+}
+
+
+bool Automaton::make_transition(const char& input_char, const size_t& current_index)
+{
+    if (state == int(pattern.length() - 1))
+        return true;
+    else if (state == loop_state && input_char == loop_char)
+    {
+        start_index = current_index - loop_state;
+    }
+    else if (input_char == pattern[state + 1])
+        state++;
+    else
+    {
+        state = -1;
+        start_index = current_index + 1;
+    }
+    return false;
+}
+
+size_t Automaton::find_first_match(const std::string& str)
+{
+    size_t i = 0;
+    bool done = false;
+    while (!done && i < str.length())
+    {
+        done = make_transition(str[i], i);
+        i++;
+    }
+    return start_index;
+}
+
+int main()
+{
+    std::string p = "hack";
+    std::string s = "hhhackerrank";
+    Automaton a = Automaton(p);
+    auto first_match = a.find_first_match(s);
+    std::cout << "Match found at index: " << first_match << std::endl;
+    return 0;
+}
